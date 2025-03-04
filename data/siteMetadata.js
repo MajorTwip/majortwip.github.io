e9916b945cddcbebafe1fa02c6999e813a0f6fb8
MajorTwip <majortwip@twipnet.ch>
2025-03-04 12:57:10 +0100

    Update siteMetadata.js

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,10 +1,10 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
+  title: 'COMSTOCK Log',
+  author: 'MajorTwip',
+  headerTitle: 'Comstock.ch',
   description: 'A blog created with Next.js and Tailwind.css',
-  language: 'en-us',
+  language: 'de-ch',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
   siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
