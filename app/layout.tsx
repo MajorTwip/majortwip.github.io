06a8015e9be98e3a2030cea652d4d2e86c4a237c
MajorTwip <majortwip@comstock.ch>
2025-03-11 16:52:09 +0100

    go

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,6 +11,7 @@ import Footer from '@/components/Footer'
 import siteMetadata from '@/data/siteMetadata'
 import { ThemeProviders } from './theme-providers'
 import { Metadata } from 'next'
+import { ElfsightWidget } from '@/components/ElfsightWidget'
 
 const space_grotesk = Space_Grotesk({
   subsets: ['latin'],
@@ -103,6 +104,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
               <main className="mb-auto">{children}</main>
             </SearchProvider>
             <Footer />
+            <ElfsightWidget widgetId={'b1ee90e1-79dd-45f9-abfe-49772c936355'} />
           </SectionContainer>
         </ThemeProviders>
       </body>
---
+++ components/ElfsightWidget.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+import dynamic from 'next/dynamic';
+import { ElfsightWidgetProps } from 'react-elfsight-widget';
+
+export const ElfsightWidget = dynamic<ElfsightWidgetProps>(
+  async () => (await import('react-elfsight-widget')).ElfsightWidget,
+  {
+    ssr: false
+  }
+);
