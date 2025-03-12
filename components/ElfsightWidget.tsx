60e8e57a732920e7685b9ea5cd425db7ae24e847
MajorTwip <majortwip@twipnet.ch>
2025-03-12 07:36:29 +0100

    lint

--- components/ElfsightWidget.tsx
+++ components/ElfsightWidget.tsx
@@ -1,11 +1,11 @@
-"use client"
+'use client'
 
-import dynamic from 'next/dynamic';
-import { ElfsightWidgetProps } from 'react-elfsight-widget';
+import dynamic from 'next/dynamic'
+import { ElfsightWidgetProps } from 'react-elfsight-widget'
 
 export const ElfsightWidget = dynamic<ElfsightWidgetProps>(
   async () => (await import('react-elfsight-widget')).ElfsightWidget,
   {
-    ssr: false
+    ssr: false,
   }
-);
+)
